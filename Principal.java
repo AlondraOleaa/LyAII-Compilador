d2d8e87ff89d60c9274bdac3de9c25f1b19405cc
Alondra Yamilet Olea Cardenas <158488326+AlondraOleaa@users.noreply.github.com>
2026-06-29 00:43:10 +0000

    primera mejora

--- Principal.java
+++ Principal.java
@@ -1,9 +1,24 @@
-import java.io.IOException;
-
+import java.io.*;
+import java.nio.file.Files;
+import javax.swing.*;
+//Mejora 1: Abrir archivos
 public class Principal {
     public static void main(String[] args) throws IOException {
-        String codigo = "var1 int ; var2 int ; var1 := var2 + var1 ; print var1 + var2 ;  ";
-        new Parser(codigo);
-    }
 
+        JFileChooser selecArchivo = new JFileChooser();
+        selecArchivo.setDialogTitle("Seleccione un archivo de código");
+
+        int opcion = selecArchivo.showOpenDialog(null);
+
+        if(opcion == JFileChooser.APPROVE_OPTION){
+            File archivo = selecArchivo.getSelectedFile();
+            String codigo = Files.readString(archivo.toPath());
+            System.out.println("==================== CÓDIGO FUENTE (" + archivo.getName() + ") ====================");
+            System.out.println(codigo);
+            System.out.println("==================== FIN CÓDIGO FUENTE ====================");
+            new Parser(codigo);
+        }else{
+            JOptionPane.showMessageDialog(null,"No se seleccionó ningún archivo.");
+        }
+    }
 }
